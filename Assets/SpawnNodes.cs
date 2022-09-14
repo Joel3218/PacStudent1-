4104cb2423071fec7cc3bd51a1fe59b1c8dd677b
Joel3218 <13642499@student.uts.edu.au>
2022-09-14 15:19:22 +1000

    Script for spawning nodes

--- Assets/SpawnNodes.cs
+++ Assets/SpawnNodes.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class SpawnNodes : MonoBehaviour
 {
+
+    int num = 25;
+    public float offset = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (gameObject.name == "Node")
+        {
+            for (int i = 0; i < num; i++)
+            {
+                //cloning the node
+                GameObject clone = Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+
+            }
+        }
     }
 
     // Update is called once per frame
