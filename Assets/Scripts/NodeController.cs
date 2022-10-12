859eb1731d7abaece04455552e19d2341d570653
Joel3218 <13642499@student.uts.edu.au>
2022-10-12 13:35:56 +1100

    fixed pellet issue

--- Assets/Scripts/NodeController.cs
+++ Assets/Scripts/NodeController.cs
@@ -131,7 +131,7 @@ public class NodeController : MonoBehaviour
         }
     }
 
-    private void onTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player" && pelletNode)
         {
