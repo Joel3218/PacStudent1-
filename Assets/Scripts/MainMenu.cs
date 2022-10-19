a6ccedaf0e2c177d5b9dc3dfeebb37dc473099f9
Joel3218 <13642499@student.uts.edu.au>
2022-10-19 16:02:15 +1100

    adding scripts to change scenes

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string level1Scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Level1()
+    {
+
+    }
+
+    public void Level2()
+    {
+
+    }
+}
