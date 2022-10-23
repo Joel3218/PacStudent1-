c1b07cbdb0d816e276c93800adbdad997760279d
Joel3218 <13642499@student.uts.edu.au>
2022-10-24 10:18:40 +1100

    adding an exit button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string level1Scene;
+    public string level0Scene;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,4 +28,10 @@ public class MainMenu : MonoBehaviour
     {
 
     }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene(level0Scene);
+    }
 }
+
